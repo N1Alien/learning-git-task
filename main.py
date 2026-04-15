f713208c3bf8e836b321a94a242b34fd176b6365
N1Alien <n1anoobis@gmail.com>
2026-04-15 13:14:21 +0200

    Add for loop to handle task

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ shooping = {
     "bakery": ["bread", "buns", "doughnut"],
     "greengrocer": ["carrots", "celery", "rocket"],
 }
+
+
+for shop, items in shooping.items():
+    print(
+        f"I'm going to the {shop.capitalize()} and buy there {', '.join(item.capitalize() for item in items).split()}."
+    )
+print(f"I buy {sum(len(items) for items in shooping.values())} products in total.")
